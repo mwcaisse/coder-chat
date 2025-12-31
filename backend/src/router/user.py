093427568322b1139b988e54d2fdb682c18354c1
Mitchell Caisse <mitchell@mwcaisse.com>
2025-12-31 18:58:35 -0500

    returned a proper response + added error handling

--- backend/src/router/user.py
+++ backend/src/router/user.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+import json
+
+from fastapi import APIRouter, Response, status
 
 from src.database import DatabaseSessionDepend
+from src.exceptions import ValidationError
 from src.models.user import CreateNewUserModel
 from src.services.user import create_user
 
@@ -9,4 +12,13 @@ router = APIRouter()
 
 @router.post("/user/")
 def create_user_r(user: CreateNewUserModel, db: DatabaseSessionDepend):
-    create_user(user, db)
+    try:
+        create_user(user, db)
+    except ValidationError as e:
+        # TODO: This should be an app wide handler / middleware, but this works for now
+        return Response(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=json.dumps({"error": str(e)}),
+        )
+
+    return Response(status_code=status.HTTP_201_CREATED)
